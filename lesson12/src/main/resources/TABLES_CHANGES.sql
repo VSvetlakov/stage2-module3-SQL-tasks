f651ec7a0174fc001f85536a3ae4675b1924981a
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2024-02-03 09:59:40 +0400

    v.2

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,6 +1,8 @@
-alter table MARK  drop constraint CONSTRAINT_23;
-alter table MARK add foreign key (STUDENT_ID) references STUDENT on delete cascade;
-alter table PAYMENT drop constraint CONSTRAINT_FBE;
-alter table PAYMENT add foreign key (STUDENT_ID) references STUDENT on delete cascade;
-alter table PAYMENT drop constraint CONSTRAINT_FB;
-alter table PAYMENT add foreign key (TYPE_ID) references PAYMENTTYPE on delete cascade;
+alter table MARK drop constraint MARK_STUDENT_ID_FKEY;
+alter table MARK add constraint MARK_STUDENT_ID_FKEY foreign key (STUDENT_ID) references STUDENT on delete cascade;
+alter table PAYMENT drop constraint PAYMENT_STUDENT_ID_FKEY;
+alter table PAYMENT add constraint PAYMENT_STUDENT_ID_FKEY foreign key (STUDENT_ID) references STUDENT on delete cascade;
+alter table PAYMENT drop constraint PAYMENT_TYPE_ID_FKEY;
+alter table PAYMENT add constraint PAYMENT_TYPE_ID_FKEY foreign key (TYPE_ID) references PAYMENTTYPE on delete cascade;
+
+
